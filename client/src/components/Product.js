37f8876122cf37c65d5eace908b875823d2849a6
Robert Nugent <d00244618@student.dkit.ie>
2023-03-20 02:39:52 +0000

    quantity bug fix

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -107,6 +107,7 @@ export default class Product extends Component {
     const email = localStorage.email
     let photos = null;
     const quantity = this.state.quantityNum - 1;
+    localStorage.cart_item = localStorage.cart_item + quantity
     console.log("Quantity: " + quantity)
     if (this.state.embeddedImage === true) {
       photos = JSON.stringify(this.state.shoe.photos)
